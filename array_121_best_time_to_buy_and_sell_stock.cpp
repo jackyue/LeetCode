14a93c3b1b38e5fe0f979f37d543a2a771b3fa81
jackyue <yuebao332@126.com>
2018-04-27 01:31:15 +0800

    Update array_121_best_time_to_buy_and_sell_stock.cpp

--- array_121_best_time_to_buy_and_sell_stock.cpp
+++ array_121_best_time_to_buy_and_sell_stock.cpp
@@ -14,9 +14,11 @@ public:
         int max_profit = 0;
         int min = prices[0];
         for (uint32_t i = 1; i < prices.size(); ++i) {
+            // 更新买入价
             if (prices[i] < min) {
                 min = prices[i];
             }
+            // 当前价格高于买入价并且差值大于最大利润则更新最大利润
             if (prices[i] > min
                     && prices[i] - min > max_profit) {
                 max_profit = prices[i] - min;
